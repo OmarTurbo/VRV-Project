046c7a4ec4cff574cc3af2450663b159ed074e4b
Omar Ahmed Mahmoud Mohamed <86806282+OmarTurbo@users.noreply.github.com>
2024-01-09 07:22:43 -0800

    adding some edits

--- javaScript/main.js
+++ javaScript/main.js
@@ -38,23 +38,23 @@ $(document).ready(() => {
 
 
 
-// changing Navbar
-var prevScrollpos = window.scrollY;
-window.onscroll = function () {
-    var currentScrollPos = window.scrollY;
-    if (prevScrollpos > currentScrollPos) {
-        document.getElementById("navbar").style.top = "0";
-        $('nav').css('backgroundColor', '#00000050');
-        $('nav a').css('color', '#fff')
-    } else {
-        document.getElementById("navbar").style.top = "-180px";
-        $('nav').css('backgroundColor', 'transparent');
-        $('nav a').css('color', '#fff')
-        prevScrollpos = 0;
-    }
+// // changing Navbar
+// var prevScrollpos = window.scrollY;
+// window.onscroll = function () {
+//     var currentScrollPos = window.scrollY;
+//     if (prevScrollpos > currentScrollPos) {
+//         document.getElementById("navbar").style.top = "0";
+//         $('nav').css('backgroundColor', '#00000050');
+//         $('nav a').css('color', '#fff')
+//     } else {
+//         document.getElementById("navbar").style.top = "-180px";
+//         $('nav').css('backgroundColor', 'transparent');
+//         $('nav a').css('color', '#fff')
+//         prevScrollpos = 0;
+//     }
 
-    prevScrollpos = currentScrollPos;
-}
+//     prevScrollpos = currentScrollPos;
+// }
 
 const swiper = new Swiper(
     '.swiper-container', {
